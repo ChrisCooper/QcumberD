b964af10f39e83a6735c0123c37484d7cd94e92a
Chris Cooper <chriscooper1991@gmail.com>
2012-09-24 07:51:36 -0400

    Added view for courses by name

--- QcumberD/course_catalog/urls.py
+++ QcumberD/course_catalog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('course_catalog.views',
     url(r'^$', 'index'),
     url(r'^course/(?P<course_id>\d+)/$', 'course_detail'),
+    url(r'^course/(?P<subject_abbr>\w+)_(?P<course_number>\w+)/$', 'course_detail'),
     url(r'^experiments$', 'experiments'),
 )
--- QcumberD/course_catalog/views.py
+++ QcumberD/course_catalog/views.py
@@ -7,8 +7,12 @@ def index(request):
     course_list = Course.objects.all().order_by('number')[:5]
     return render_to_response('course_catalog/index.html', {'course_list': course_list})
 
-def course_detail(request, course_id):
-    c = get_object_or_404(Course, pk=course_id)
+def course_detail(request, course_id=None, subject_abbr=None, course_number=None):
+    c = None
+    if course_id is not None:
+        c = get_object_or_404(Course, pk=course_id)
+    else:
+        c = get_object_or_404(Course, subject__abbreviation=subject_abbr.upper(), number=course_number)
     return render_to_response('course_catalog/course_detail.html', {'course': c})
 
 
