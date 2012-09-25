ef0b898bac69691e21b8c31716679bd468dd3ebb
Chris Cooper <chriscooper1991@gmail.com>
2012-09-24 20:50:38 -0400

    Factored Qcumber page out of course and subject views

--- QcumberD/course_catalog/views.py
+++ QcumberD/course_catalog/views.py
@@ -4,17 +4,17 @@ from course_catalog.models import Course, Subject
 
 def index(request):
     course_list = Course.objects.all().order_by('number')[:5]
-    return render_to_response('course_catalog/index.html', {'course_list': course_list})
+    return render_to_response('course_catalog/pages/index.html', {'course_list': course_list})
 
 def course_detail(request, subject_abbr=None, course_number=None):
     c = get_object_or_404(Course, subject__abbreviation__iexact=subject_abbr, number=course_number)
-    return render_to_response('course_catalog/course_detail.html', {'course': c})
+    return render_to_response('course_catalog/pages/course_detail.html', {'course': c})
 
 
 def subject_detail(request, subject_abbr):
     s = get_object_or_404(Subject, abbreviation__iexact=subject_abbr)
     c = s.courses.order_by('number')
-    return render_to_response('course_catalog/subject_detail.html', {'subject': s,
+    return render_to_response('course_catalog/pages/subject_detail.html', {'subject': s,
                                                                      'courses': c})
 
 
