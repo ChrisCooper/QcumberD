b5d8936542077a1004f0659959447136fb07458e
Chris Cooper <chriscooper1991@gmail.com>
2012-10-23 08:18:29 -0400

    Home link uses a named url

--- QcumberD/course_catalog/urls.py
+++ QcumberD/course_catalog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('course_catalog.views',
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name="home"),
     url(r'^course/(?P<subject_abbr>\w+)_(?P<course_number>\w+)/$', 'course_detail'),
     url(r'^subject/(?P<subject_abbr>\w+)/$', 'subject_detail'),
     url(r'^search/$', 'search'),
