8cfade214579ee902079ec2f078203dfbe9402f9
Chris Cooper <chriscooper1991@gmail.com>
2012-10-25 21:36:03 -0400

    Re-added support for 1.5-style urls

--- QcumberD/qcumber/settings.py
+++ QcumberD/qcumber/settings.py
@@ -11,6 +11,11 @@ elif qcumber.config.current.CONFIG == "scraping":
     from qcumber.config.scraping import *
 
 
+#support 1.5-style urls
+import django.template
+django.template.add_to_builtins('django.templatetags.future')
+
+
 MANAGERS = ADMINS
 
 SCRAPER_CONFIG_FILE = unixy_project_path("ignored_files/selenium_config.txt")
