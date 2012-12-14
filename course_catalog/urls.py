4216d031f86bf140436b5aa5ad04cf019c29afca
Chris Cooper <chriscooper1991@gmail.com>
2012-12-14 15:50:32 -0500

    Added TOS and facebook channel.html

--- course_catalog/urls.py
+++ course_catalog/urls.py
@@ -7,5 +7,9 @@ urlpatterns = patterns('course_catalog.views',
     url(r'^courses/(?P<subject_abbr>\w+)_(?P<course_number>\w+)/$', 'course_detail'),
     url(r'^subjects/(?P<subject_abbr>\w+)/$', 'subject_detail'),
     url(r'^search/$', 'search'),
+
+
+    url(r'^channel.html$', 'facebook_channel'),
+    url(r'^tos$', 'tos'),
     url(r'^experiments$', 'experiments'),
 )
