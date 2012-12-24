c6406e964a6abe4a4197f1c459dd6a8bd5ad5898
Chris Cooper <chriscooper1991@gmail.com>
2012-12-24 15:12:46 -0500

    Expanded enrollment numbers to everyone

--- course_catalog/views.py
+++ course_catalog/views.py
@@ -15,7 +15,7 @@ def index(request):
     subject_list = Subject.objects.all().order_by('abbreviation')
     return render_to_response('course_catalog/pages/index.html', {'subject_list': subject_list})
 
-#@cache_page(60 * 30)
+@cache_page(60 * 30)
 def course_detail(request, subject_abbr=None, course_number=None):
     c = get_object_or_404(Course, subject__abbreviation__iexact=subject_abbr, number=course_number)
     terms = [s.term for s in c.sections.distinct('term')]
