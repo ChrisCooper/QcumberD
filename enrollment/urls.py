a9959407c2fb312f8d74e71b96a5c7c73a735f9f
Chris Cooper <chriscooper1991@gmail.com>
2012-12-24 00:07:20 -0500

    Proper URL and response format for enrollment numbers

--- enrollment/urls.py
+++ enrollment/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('enrollment.views',
-    url(r'^$', 'proof'),
+    url(r'^(?P<subject_abbr>\w+)/(?P<course_num>\w+)/(?P<solus_id>\w+)/$', 'enrollment_numbers'),
+
 )
--- enrollment/views.py
+++ enrollment/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 
+import json
+
 from enrollment.models import SolusSession
 from course_catalog.models import Section
 
-def proof(request):
-
-    #section = get_object_or_404(Section, course__subject__abbreviation='CHEM', course__number='112A', solus_id='1759')
-    #section = get_object_or_404(Section, course__subject__abbreviation='FREN', course__number='110B', solus_id='4784')
-    section = get_object_or_404(Section, course__subject__abbreviation='MINE', course__number='244', solus_id='5916')
+def enrollment_numbers(request, subject_abbr, course_num, solus_id):
+    section = get_object_or_404(Section, course__subject__abbreviation=subject_abbr, course__number=course_num, solus_id=solus_id)
 
     capacity, enrolled = SolusSession().scrape_enrollment(section)
 
-    return HttpResponse("%s/%s" % (enrolled, capacity))
+    return HttpResponse(json.dumps({'enrolled': enrolled, 'capacity': capacity}))
