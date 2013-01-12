c4a24d1e3e543c6502774d5c191113fe7fbacd6f
pR0Ps <pR0Ps.CM@gmail.com>
2013-01-12 18:49:01 -0500

    Parsing out course names and links

--- scraper/textbook_scraper.py
+++ scraper/textbook_scraper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 class TextbookScraper(object):
@@ -9,4 +10,15 @@ class TextbookScraper(object):
     def testing(self):
 
         r = requests.get("http://www.campusbookstore.com/Textbooks/Booklists/")
-        return r.text
+
+        b = BeautifulSoup(r.text)
+        content = b.find("div", {"class":"thecontent"})
+        links  = content.find_all("a")
+
+        temp = []
+
+        for link in links:
+            if "campusbookstore.com/Textbooks/Course/" in link.attrs.get("href", ""):
+                temp.append((link.string, link.attrs["href"]))
+
+        return "</br>".join([n + " ---> " + l for n, l in temp])
