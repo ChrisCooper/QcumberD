6fde93c6c7b6e603299319453380b3c1be3697ea
pR0Ps <pR0Ps.CM@gmail.com>
2013-01-29 23:31:17 -0500

    Fixed ordering of scrape targets. Added detection of SOLUS data integrity errors

--- scraper/solus_session.py
+++ scraper/solus_session.py
@@ -42,48 +42,46 @@ class SolusSession(object):
         # Go to the course catalog after logging in
         self._catalog_post("")
 
-        return response
-
     def select_alphanum(self, alphanum):
         """Navigates to a letter/number"""
-        return self._catalog_post('DERIVED_SSS_BCC_SSR_ALPHANUM_' + alphanum.upper())
+        self._catalog_post('DERIVED_SSS_BCC_SSR_ALPHANUM_' + alphanum.upper())
 
     def dropdown_subject(self, abbr, title):
         """Opens the dropdown menu for a subject"""
         action = self.parser().subject_dropdown(abbr, title)
-
-        return self._catalog_post(action)
+        self._catalog_post(action)
 
     def select_course(self, number):
         """Clicks on a course"""
         action = self.parser().course_link(number)
-        return self._catalog_post(action)
+        self._catalog_post(action)
 
     def view_section(self, class_num):
         """Clicks on a course section"""
         action = self.parser().section_link(class_num)
-        return self._catalog_post(action)
+        self._catalog_post(action)
 
     def show_sections(self):
         """Clicks on the 'View class sections' button on the course page"""
-        return self._catalog_post('DERIVED_SAA_CRS_SSR_PB_GO')
+        #action = self.parser().show_sections()
+        self._catalog_post('DERIVED_SAA_CRS_SSR_PB_GO')
 
     def switch_terms(self, year, season):
         """Shows the sections for a term of the class"""
         term_key = self.parser().term_key(year, season)
-        return self._catalog_post('DERIVED_SAA_CRS_SSR_PB_GO$92$', extras={'DERIVED_SAA_CRS_TERM_ALT': term_key})
+        self._catalog_post(action='DERIVED_SAA_CRS_SSR_PB_GO$92$', extras={'DERIVED_SAA_CRS_TERM_ALT': term_key})
 
     def show_all_sections(self):
         """Clicks the 'view all' button"""
-        return self._catalog_post('CLASS_TBL_VW5$fviewall$0')
+        self._catalog_post('CLASS_TBL_VW5$fviewall$0')
 
     def return_from_section(self):
         """Navigates back from section to course"""
-        return self._catalog_post('CLASS_SRCH_WRK2_SSR_PB_CLOSE')
+        self._catalog_post('CLASS_SRCH_WRK2_SSR_PB_CLOSE')
 
     def return_from_course(self):
         """Navigates back from course to subject"""
-        return self._catalog_post('DERIVED_SAA_CRS_RETURN_PB')
+        self._catalog_post('DERIVED_SAA_CRS_RETURN_PB')
 
     def parser(self):
         """Returns a SolusParser instance for the current page"""
@@ -91,6 +89,12 @@ class SolusSession(object):
 
     def _catalog_post(self, action, extras={}):
         """Submits a post request to the site"""
+        if not action:
+            return
+
         extras['ICAction'] = action
         self.latest_response = self.session.post(self.course_catalog_url, data=extras)
         self.latest_text = self.latest_response.text
+
+        if "Data Integrity Error" in self.latest_text:
+            raise Exception("SOLUS reported a Data Integrity Error")
