fac7f8567479551e3bb572a5b805fb88badb2707
Chris Cooper <chriscooper1991@gmail.com>
2013-01-29 20:54:15 -0500

    Added old scraper reference code

---
+++ scraper/old scraper/__init__.py
@@ -0,0 +1,89 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from selenium import selenium
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import time
+
+from solus_scraper.models import JobConfig, section_types, weekdays
+import course_catalog.models
+
+import login_helper
+import subject_helper
+
+import re
+
+class ScraperTools(object):
+    def __init__(self, selen, config):
+        self.selen = selen
+        self.config = config
+
+        # The attribute names created by extra info of certain names
+        self.extra_info_mappings = {"Career": "career",
+                                    "Typically Offered": "typically_offered",
+                                    "Units": "units",
+                                    "Grading Basis": "grading_basis",
+                                    "Add Consent": "add_consent",
+                                    "Course Components": "course_componenets",
+                                    "Drop Consent": "drop_consent",
+                                    "Enrollment Requirement": "enrollment_requirement",
+                                    }
+    
+    def wait_for_page(self):
+        self.selen.wait_for_page_to_load(self.config.timeout_milliseconds)
+
+    def wait_then_click(self, identifier):
+        """
+        Tries to find an element to click, and if it's not present,
+        waits with exponential back-off, then tries again 
+        """
+        wait_time = 0.5
+
+        while not sel.is_element_present(identifier):
+            print("Element not present. Waiting %f seconds..." % wait_time)
+            time.sleep(wait_time)
+            wait_time *= 2
+        time.sleep(3)
+        sel.click(identifier)
+
+
+
+def full_scrape(config):
+    print("Updating constant entries...")
+    update_constants()
+
+    print("Beginning scrape job...")
+
+    # Create a new instance of Selenium
+    s = selenium("localhost", 4444, "*chrome", "https://sso.queensu.ca/amserver/UI/Login")
+    s.start()
+        
+    s.set_timeout(config.timeout_milliseconds)
+
+    #save our useful stuff to pass around
+    tools = ScraperTools(s, config)
+
+    #get to a browsable state
+    login_helper.navigate_to_course_catalog(tools)
+
+    #Go through the pages for each letter in the course catalogue
+
+    for letter in config.subject_letters:
+        subject_helper.drill_subjects_for_letter(letter, tools)
+
+    print("Completed scrape job")
+
+    s.stop()
+
+   
+def update_constants():
+    for type_abbr in section_types:
+        type = course_catalog.models.existing_or_new(course_catalog.models.SectionType, abbreviation=type_abbr)
+        type.name = section_types[type_abbr]
+        type.save()
+
+    for day_abbr in weekdays:
+        day = course_catalog.models.existing_or_new(course_catalog.models.DayOfWeek, abbreviation=day_abbr)
+        day.name = weekdays[day_abbr]
+        day.save()
---
+++ scraper/old scraper/login_helper.py
@@ -0,0 +1,49 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from django.conf import settings
+
+def navigate_to_course_catalog(tools):
+    s = tools.selen
+
+    # go to the solus login page
+    print "Opening login page..."
+    s.open("https://sso.queensu.ca/amserver/UI/Login")
+
+    #Get login information from config file
+    with open(settings.SCRAPER_CONFIG_FILE, 'r') as config_file:
+        line_num = 0
+        login_info = ['','']
+        for line in config_file:
+            login_info[line_num] = line.strip()
+            line_num += 1
+
+    #type username
+    s.type("id=IDToken1", login_info[0])
+    #type password
+    s.type("id=IDToken2", login_info[1])
+
+    #Click the log in button
+    s.click("name=Login.Submit")
+
+    #Wait for the portal page to load
+    tools.wait_for_page()
+
+    #Get URL for SOLUS and open it
+    solus_url = s.get_attribute("link=SOLUS Student Centre@href")
+    s.open(solus_url)
+
+    #Get the content frame
+    s.select_frame("name=TargetContent")
+
+    #"Search For Classes"
+    s.click("id=DERIVED_SSS_SCL_SSS_GO_4$230$")
+
+    tools.wait_for_page()
+
+    #"browse course catalog"
+    s.click("link=browse course catalog")
+    tools.wait_for_page()
+
+    print "Navigation to SOLUS course catalog complete."
