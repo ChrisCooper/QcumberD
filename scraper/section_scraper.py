3ae4377ee2cf8a9929b1461a86a546cc72abfe8c
Chris Cooper <chriscooper1991@gmail.com>
2013-02-03 02:55:13 -0500

    Prerequisite links are parsed

---
+++ scraper/section_scraper.py
@@ -0,0 +1,17 @@
+from solus_scraper import SolusScraper
+#from section_component_scraper import SectionComponentScraper
+
+class SectionScraper(SolusScraper):
+
+    def scrape_all_sections(self, course):
+        """Scrapes all sections for the current course"""
+
+        # for term in terms:
+        #     select term
+        #     view all sections
+        #     scrape sections
+        #         if deepscrape:
+        #             visit section page
+
+
+        return
