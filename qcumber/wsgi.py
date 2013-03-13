671bc9357d2500ad87f962a6a6164a77598270e6
Chris Cooper <chriscooper1991@gmail.com>
2013-03-12 22:25:03 -0400

    Changed wsgi comment

--- qcumber/wsgi.py
+++ qcumber/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mentoring project.
+WSGI config for Qcumber.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
