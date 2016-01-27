1089d4022813b924cf46d03277dca7cb540df055
Chris Cooper <chris@quixotic-chris.com>
2016-01-26 21:48:56 -0500

    remove some outdated files

--- wipe_cache.sh
+++
@@ -1,8 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-kill $(cat $HOME/memcached.pid)
-rm $HOME/memcached.sock
-rm $HOME/memcached.pid
-memcached -d -m 40 -s $HOME/memcached.sock -P $HOME/memcached.pid 
